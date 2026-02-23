67b3023bc4182cfab8e8d53995c1ff642a5fabdd
cachinhosfxp <joao.fernandes.lima24@escola.pr.gov.br>
2026-02-23 20:53:49 -0300

    teeste 1

---
+++ script.js
@@ -0,0 +1,5 @@
+function adoteAgora() {
+    alert("Obrigado por escolher adotar! Visite nossa seção de adoção responsável.");
+    // Aqui você pode redirecionar o usuário para a página de adoção:
+    // window.location.href = "seu-link-para-adoção.html";
+}
